7052029169280ac9942d0ddc703c355f73e0dadc
SeanRog <seanmikerog@gmail.com>
2020-04-12 14:33:37 -0700

    Second Commit

---
+++ QuirkyBookRental/Controllers/GenreController.cs
@@ -0,0 +1,133 @@
+﻿using QuirkyBookRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuirkyBookRental.Controllers
+{
+    public class GenreController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public GenreController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        // GET: Genre
+        public ActionResult Index()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        //get Action
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Genre genre)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Genres.Add(genre);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Genre genre = db.Genres.Find(id);
+            if(genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Genre genre)
+        {
+            if(ModelState.IsValid)
+            {
+                //less expensive if there's a lot of columns
+                //var GenreInDb = db.Genres.FirstOrDefault(g => g.Id.Equals(genre.Id)); //gets genre based on genre.Id, then sets GenreInDb to that genre object
+                //GenreInDb.name = genre.name;
+
+                //expensive if there's a lot of columns
+                db.Entry(genre).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                //less expensive if there's a lot of columns
+                //var GenreInDb = db.Genres.FirstOrDefault(g => g.Id.Equals(genre.Id));
+                //GenreInDb.name = genre.name;
+
+                //expensive if there's a lot of columns
+                Genre genre = db.Genres.Find(id);
+                db.Genres.Remove(genre);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+    }
+}
